77f6c94fdae7de13379e1b55fdba2219a140ce93
am-garc1a <am.garcia@globant.com>
2022-12-09 13:08:09 -0500

    runners úpdate

--- src/test/java/runners/MobileTestRunner.java
+++ src/test/java/runners/MobileTestRunner.java
@@ -1,4 +1,14 @@
 package runners;
 
-public class MobileTestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+/**
+ * Runner class for mobile feature.
+ */
+@CucumberOptions(
+        features = "src/test/java/tests/features",
+        glue = {"tests.steps.mobile"},
+        tags = "@DisneyNavigation")
+public class MobileTestRunner extends AbstractTestNGCucumberTests {
 }
--- src/test/java/runners/WebTestRunner.java
+++ src/test/java/runners/WebTestRunner.java
@@ -3,9 +3,12 @@ package runners;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
+/**
+ * Runner class for web feature.
+ */
 @CucumberOptions(
         features = "src/test/java/tests/features",
-        glue = {"tests.steps"},
+        glue = {"tests.steps.web"},
         tags = "@EspnNavigation")
 public class WebTestRunner extends AbstractTestNGCucumberTests {
 }

05cc7484cc10d5dba7c1b07036a48c51e2a4ca60
am-garc1a <am.garcia@globant.com>
2022-12-09 17:31:59 -0500

    mobile driver fix

--- src/test/java/tests/steps/mobile/DisneySteps.java
+++ src/test/java/tests/steps/mobile/DisneySteps.java
@@ -1,4 +1,25 @@
 package tests.steps.mobile;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class DisneySteps {
+    @Given("User is in Dashboard screen")
+    public void userIsInDashboardScreen() {
+    }
+
+    @Given("User is in map screen")
+    public void userIsInMapScreen() {
+        
+    }
+
+    @When("User clicks category list button")
+    public void userClicksCategoryListButton() {
+        
+    }
+
+    @Then("Categories, including hotels option should be displayed")
+    public void categoriesIncludingHotelsOptionShouldBeDisplayed() {
+    }
 }
